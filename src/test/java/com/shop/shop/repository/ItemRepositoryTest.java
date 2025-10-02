6ffc8e72fa2fb82ebcedb4a114d40a6e341f690b
권태욱 <notebook@>
2025-10-02 20:30:27 +0900

    10/02

--- src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
@@ -3,11 +3,13 @@ package com.shop.shop.repository;
 
 import com.shop.shop.constant.ItemSellStatus;
 import com.shop.shop.entity.Item;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +23,7 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품명 생성 테스트")
+    @BeforeEach
     public void createItemTest() {
         for (int i = 0; i < 10; i++) {
             Item item = new Item();
@@ -38,21 +41,32 @@ public class ItemRepositoryTest {
 
     @Test
     public void 상품명조회테스트() {
-        this.createItemTest();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품 1");
 
-        for(Item item : itemList) {
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
 
     @Test
-    public void 상품명or상품상세설명테스트(){
-        this.createItemTest();
-        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품 5", "테스트 상품 상세 설명 1")
-        for(Item item : itemList) {
+    public void 상품명or상품상세설명테스트() {
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품 5", "테스트 상품 상세 설명 1");
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+}
+
+class createitem{
 
 }
